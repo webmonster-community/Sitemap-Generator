f965f963b2676c38158031da4e01367fcd2d7d36
Webmonster Antilles <110256555+WebmonsterA@users.noreply.github.com>
2022-08-06 22:59:26 -0400

    Create sitemap.php

---
+++ php/sitemap.php
@@ -0,0 +1,119 @@
+<?php
+/**
+ * WEBMONSTER ANTILLES
+ * SITEMAP GENERATOR
+ * LANGUAGE : PHP
+ * VERSION 1.0.0
+ * DATE 2022-08-06
+ * LICENCE : MIT
+
+ * NEED : PHP5+ - MYSQL/MARIADB - LINUX
+
+ **/
+
+header('Content-Type: text/xml; charset=UTF-8');
+
+/** CONFIGURATION */
+$url = 'https://mydomain.com';
+$dsn = "mysql:host=localhost;dbname=";
+$usr = "user12";
+$pwd = "12user";
+$dbn = "db";
+
+
+// DO NOT TOUCH
+// OPEN DATABASE CONNECTION
+$pdo = new PDO($dsn.$dbn, $usr, $pwd);
+
+
+/*
+SLUGIFY
+You need to create proper url
+You can load data from your database for example title for a blog or product list
+title = You Can Do It Baby!
+slugify = you-can-do-it-baby
+It is better for SEO
+*/
+function slugify($str, $options = array()) {
+
+    $str = mb_convert_encoding((string)$str, 'UTF-8', mb_list_encodings());
+    $str = str_ireplace("&#39;","-", $str);
+
+    $defaults = array(
+        'delimiter' => '-',
+        'limit' => null,
+        'lowercase' => true,
+        'replacements' => array(),
+        'transliterate' => true,
+    );
+    $options = array_merge($defaults, $options);
+    $char_map = array(
+        'À' => 'A', 'Á' => 'A', 'Â' => 'A', 'Ã' => 'A', 'Ä' => 'A', 'Å' => 'A', 'Æ' => 'AE', 'Ç' => 'C',
+        'È' => 'E', 'É' => 'E', 'Ê' => 'E', 'Ë' => 'E', 'Ì' => 'I', 'Í' => 'I', 'Î' => 'I', 'Ï' => 'I',
+        'Ð' => 'D', 'Ñ' => 'N', 'Ò' => 'O', 'Ó' => 'O', 'Ô' => 'O', 'Õ' => 'O', 'Ö' => 'O', 'Ő' => 'O',
+        'Ø' => 'O', 'Ù' => 'U', 'Ú' => 'U', 'Û' => 'U', 'Ü' => 'U', 'Ű' => 'U', 'Ý' => 'Y', 'Þ' => 'TH',
+        'ß' => 'ss',
+        'à' => 'a', 'á' => 'a', 'â' => 'a', 'ã' => 'a', 'ä' => 'a', 'å' => 'a', 'æ' => 'ae', 'ç' => 'c',
+        'è' => 'e', 'é' => 'e', 'ê' => 'e', 'ë' => 'e', 'ì' => 'i', 'í' => 'i', 'î' => 'i', 'ï' => 'i',
+        'ð' => 'd', 'ñ' => 'n', 'ò' => 'o', 'ó' => 'o', 'ô' => 'o', 'õ' => 'o', 'ö' => 'o', 'ő' => 'o',
+        'ø' => 'o', 'ù' => 'u', 'ú' => 'u', 'û' => 'u', 'ü' => 'u', 'ű' => 'u', 'ý' => 'y', 'þ' => 'th',
+        'ÿ' => 'y',
+        '©' => '(c)',
+        'Α' => 'A', 'Β' => 'B', 'Γ' => 'G', 'Δ' => 'D', 'Ε' => 'E', 'Ζ' => 'Z', 'Η' => 'H', 'Θ' => '8',
+        'Ι' => 'I', 'Κ' => 'K', 'Λ' => 'L', 'Μ' => 'M', 'Ν' => 'N', 'Ξ' => '3', 'Ο' => 'O', 'Π' => 'P',
+        'Ρ' => 'R', 'Σ' => 'S', 'Τ' => 'T', 'Υ' => 'Y', 'Φ' => 'F', 'Χ' => 'X', 'Ψ' => 'PS', 'Ω' => 'W',
+        'Ά' => 'A', 'Έ' => 'E', 'Ί' => 'I', 'Ό' => 'O', 'Ύ' => 'Y', 'Ή' => 'H', 'Ώ' => 'W', 'Ϊ' => 'I',
+        'Ϋ' => 'Y',
+        'α' => 'a', 'β' => 'b', 'γ' => 'g', 'δ' => 'd', 'ε' => 'e', 'ζ' => 'z', 'η' => 'h', 'θ' => '8',
+        'ι' => 'i', 'κ' => 'k', 'λ' => 'l', 'μ' => 'm', 'ν' => 'n', 'ξ' => '3', 'ο' => 'o', 'π' => 'p',
+        'ρ' => 'r', 'σ' => 's', 'τ' => 't', 'υ' => 'y', 'φ' => 'f', 'χ' => 'x', 'ψ' => 'ps', 'ω' => 'w',
+        'ά' => 'a', 'έ' => 'e', 'ί' => 'i', 'ό' => 'o', 'ύ' => 'y', 'ή' => 'h', 'ώ' => 'w', 'ς' => 's',
+        'ϊ' => 'i', 'ΰ' => 'y', 'ϋ' => 'y', 'ΐ' => 'i',
+        'Ş' => 'S', 'İ' => 'I', 'Ğ' => 'G', 'ş' => 's', 'ı' => 'i', 'ğ' => 'g',
+        'А' => 'A', 'Б' => 'B', 'В' => 'V', 'Г' => 'G', 'Д' => 'D', 'Е' => 'E', 'Ё' => 'Yo', 'Ж' => 'Zh',
+        'З' => 'Z', 'И' => 'I', 'Й' => 'J', 'К' => 'K', 'Л' => 'L', 'М' => 'M', 'Н' => 'N', 'О' => 'O',
+        'П' => 'P', 'Р' => 'R', 'С' => 'S', 'Т' => 'T', 'У' => 'U', 'Ф' => 'F', 'Х' => 'H', 'Ц' => 'C',
+        'Ч' => 'Ch', 'Ш' => 'Sh', 'Щ' => 'Sh', 'Ъ' => '', 'Ы' => 'Y', 'Ь' => '', 'Э' => 'E', 'Ю' => 'Yu',
+        'Я' => 'Ya',
+        'а' => 'a', 'б' => 'b', 'в' => 'v', 'г' => 'g', 'д' => 'd', 'е' => 'e', 'ё' => 'yo', 'ж' => 'zh',
+        'з' => 'z', 'и' => 'i', 'й' => 'j', 'к' => 'k', 'л' => 'l', 'м' => 'm', 'н' => 'n', 'о' => 'o',
+        'п' => 'p', 'р' => 'r', 'с' => 's', 'т' => 't', 'у' => 'u', 'ф' => 'f', 'х' => 'h', 'ц' => 'c',
+        'ч' => 'ch', 'ш' => 'sh', 'щ' => 'sh', 'ъ' => '', 'ы' => 'y', 'ь' => '', 'э' => 'e', 'ю' => 'yu',
+        'я' => 'ya',
+        'Є' => 'Ye', 'І' => 'I', 'Ї' => 'Yi', 'Ґ' => 'G',
+        'є' => 'ye', 'і' => 'i', 'ї' => 'yi', 'ґ' => 'g',
+        'Ď' => 'D', 'Ě' => 'E', 'Ň' => 'N', 'Ř' => 'R', 'Š' => 'S', 'Ť' => 'T', 'Ů' => 'U', 'ď' => 'd',
+        'ě' => 'e', 'ň' => 'n', 'ř' => 'r', 'ť' => 't', 'ů' => 'u',
+        'Ą' => 'A', 'Ć' => 'C', 'Ę' => 'e', 'Ł' => 'L', 'Ń' => 'N', 'Ś' => 'S', 'Ź' => 'Z', 'Ż' => 'Z',
+        'ą' => 'a', 'ć' => 'c', 'ę' => 'e', 'ł' => 'l', 'ń' => 'n', 'ś' => 's', 'ź' => 'z', 'ż' => 'z',
+        'Ā' => 'A', 'Č' => 'C', 'Ē' => 'E', 'Ģ' => 'G', 'Ī' => 'i', 'Ķ' => 'k', 'Ļ' => 'L', 'Ņ' => 'N',
+        'Ū' => 'u', 'Ž' => 'Z',
+        'ā' => 'a', 'č' => 'c', 'ē' => 'e', 'ģ' => 'g', 'ī' => 'i', 'ķ' => 'k', 'ļ' => 'l', 'ņ' => 'n',
+        'š' => 's', 'ū' => 'u', 'ž' => 'z', '$' => 'S'
+    );
+    $str = preg_replace(array_keys($options['replacements']), $options['replacements'], $str);
+    if ($options['transliterate']) {
+        $str = str_replace(array_keys($char_map), $char_map, $str);
+    }
+    $str = preg_replace('/[^\p{L}\p{Nd}]+/u', $options['delimiter'], $str);
+    $str = preg_replace('/(' . preg_quote($options['delimiter'], '/') . '){2,}/', '$1', $str);
+    $str = mb_substr($str, 0, ($options['limit'] ? $options['limit'] : mb_strlen($str, 'UTF-8')), 'UTF-8');
+    $str = trim($str, $options['delimiter']);
+    return $options['lowercase'] ? mb_strtolower($str, 'UTF-8') : $str;
+}
+
+// START SITEMAP HEADER
+$sitemap = "<?xml version='1.0' encoding='UTF-8'?><urlset xmlns='https://www.sitemaps.org/schemas/sitemap/0.9' xmlns:image='http://www.google.com/schemas/sitemap-image/1.1'>\n\r";
+
+// GET DATA
+$stm = $pdo->query("SELECT title FROM articles ORDER BY title;");
+$results = $stm->fetchAll(PDO::FETCH_ASSOC);
+foreach ($results AS $result):
+    $sitemap .= "<url>\r\t<loc>".$url.'/'.slugify($result->title)."/</loc>\r\t<priority>0.5</priority>\r\t<changefreq>daily</changefreq>\r</url>\n\r";
+endforeach;
+$sitemap .= '</urlset>';
+
+// WRITE SITEMAP FILE
+$sitemap_xml = './sitemap.xml';
+$file = fopen($sitemap_xml, 'w+');
+fputs($file, $sitemap);
